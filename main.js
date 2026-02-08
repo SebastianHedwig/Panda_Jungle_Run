0a0f15c7b78b34a16ed9294091b3648aa51bc935
Basti <sebastian.hedwig@web.de>
2026-02-08 15:39:14 +0100

    main.js moved to mainfolder

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-import { getPaused, initGame, setPaused } from "./core/game.js";
-import { INITIAL_MUTE_STATE_GAMESTART, SET_FULLSCREEN, SOUND_MUTE_STORAGE_KEY } from "./config/config.js";
-import { installAudioTracking } from "./app/audio/audioTracking.js";
-import { setupSoundToggle } from "./app/ui/controls/soundToggle.js";
-import { togglePauseState } from "./app/ui/controls/settingsToggle.js";
-import { setupFullscreenToggle, applyAutoFullscreen } from "./app/ui/controls/fullscreenToggle.js";
-import { setupStartScreen } from "./app/start/startScreen/startScreen.js";
-import { ViewportManagement } from "./app/ui/viewportManagement.class.js";
-import { setupMobileControls } from "./app/ui/controls/mobileControls.js";
+import { getPaused, initGame, setPaused } from "./src/core/game.js";
+import { INITIAL_MUTE_STATE_GAMESTART, SET_FULLSCREEN, SOUND_MUTE_STORAGE_KEY } from "./src/config/config.js";
+import { installAudioTracking } from "./src/app/audio/audioTracking.js";
+import { setupSoundToggle } from "./src/app/ui/controls/soundToggle.js";
+import { togglePauseState } from "./src/app/ui/controls/settingsToggle.js";
+import { setupFullscreenToggle, applyAutoFullscreen } from "./src/app/ui/controls/fullscreenToggle.js";
+import { setupStartScreen } from "./src/app/start/startScreen/startScreen.js";
+import { ViewportManagement } from "./src/app/ui/viewportManagement.class.js";
+import { setupMobileControls } from "./src/app/ui/controls/mobileControls.js";
 
 const getStoredMuted = () => {
   const stored = localStorage.getItem(SOUND_MUTE_STORAGE_KEY);
